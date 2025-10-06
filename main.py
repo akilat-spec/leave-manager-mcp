9fc9b70856536da89ebf66f89672aede44a625c6
LAPTOP-VRTTD7E8\elave <akila.t@techovirish.com>
2025-10-06 15:41:30 +0530

    MCP LeaveManager server

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def get_connection():
         host=os.environ.get("DB_HOST", "localhost"),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD", ""),
-        database=os.environ.get("DB_NAME", "leave_db"),
+        database=os.environ.get("DB_NAME", "leave_mcp"),
         port=int(os.environ.get("DB_PORT", "3306")),
     )
 
